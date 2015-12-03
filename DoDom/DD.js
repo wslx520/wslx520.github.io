b1a5fa1466a9bd0efef0ffc88e80ba4238b68fa3
李兴 <lixing@jingoal.com>
2015-12-03 13:53:21 +0800

    discard

--- DoDom/DD.js
+++ DoDom/DD.js
@@ -1,11 +1,37 @@
 var dd,
 DD = dd = function (doc) {
+    var 
+    camelCase = function (str) {
+        return str.replace(/-([a-z])/g, function (m, $1) {
+            return $1.toUpperCase();
+        })
+        // return str.replace(/-([a-z])/g, RegExp.$1.toUpperCase())
+    };
+    console.log(camelCase('i-dont-like-it'))
     function DoDom (nodes) {
-        // body...
         this.nodes = nodes;
     }
-    DoDom.prototype = {
-        
+    DoDom.prototype = {        
+        hasClass = function (elm,cls) {
+            cls = cls.split(/\s+/);
+            for(var c=cls.length,ecls = ' '+elm.className+' ';c--;) {
+                if(ecls.indexOf(' '+cls[c]+' ') == -1) {
+                    return false;
+                }
+            }
+            return true;
+        },
+        addClass = function (elm,cls) {
+            // if(!hasClass(elm,cls)) {
+                elm.className = __.trim(elm.className)+' '+cls;
+            // }
+        },
+        removeClass = function (elm,cls) {
+            cls = cls.split(/\s+/);
+            cls.forEach(function  (cc,i) {
+                elm.className = elm.className.replace(RegExp('\\b'+cc+'\\b',"g"),'');
+            })
+        },
     }
 
     return DoDom;
